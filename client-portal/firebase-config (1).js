7db80eeb7a3196e41234eb5a20b232627358581a
F2002K89 <155981146+F2002K89@users.noreply.github.com>
2026-03-16 12:47:24 +0400

    Add files via upload

---
+++ client-portal/firebase-config (1).js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDKTnzS5gK1AIX2aar4hmpUYWSnxOgRJVU",
+  authDomain:        "neft-oil-trading-portal-4ba05.firebaseapp.com",
+  projectId:         "neft-oil-trading-portal-4ba05",
+  storageBucket:     "neft-oil-trading-portal-4ba05.firebasestorage.app",
+  messagingSenderId: "374990645527",
+  appId:             "1:374990645527:web:2b1889582002d9d2aeaf52"
+};
